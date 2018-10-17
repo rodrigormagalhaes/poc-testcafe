abd1c280b2a165fc98416fdbf41fe53c08132962
Rodrigo Magalhães <rodrigorm.rm@gmail.com>
2018-10-16 23:11:12 -0300

    criação do projeto

---
+++ first-test.js
@@ -0,0 +1,10 @@
+HTML_REPORT_PATH: "./"
+
+import { Selector } from 'testcafe';
+
+fixture `Getting Started`
+    .page `http://devexpress.github.io/testcafe/example`;
+
+test('My first test', async t => {
+    // Test code
+});
